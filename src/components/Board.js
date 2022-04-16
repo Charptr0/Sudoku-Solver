6fc4f30a5563e86119bc23617485c155823cd1bb
Charptr0 <charlesofficial1@hotmail.com>
2022-04-16 16:46:32 -0400

    fixed refs

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, {useRef } from "react";
 import styles from "./board.module.css";
 import Options from "./Options";
 
@@ -19,23 +19,29 @@ function Board()
         "r1c2" : useRef(null),
     }
 
+    function solve()
+    {
+        console.log(refs.r0c0.current.value);
+    }
+
     return (
-        <div>
-            <div id={styles.game_board}>
-                <h1>Sudoku Solver</h1>
+            <div id={styles.game_board}><h1>Sudoku Solver</h1>
+            <Options solve={solve} />
                 <table> 
-                    <tbody>
-                        <tr></tr> <td><input id="r0c0" type={"number"} min={1} max={9} ref={refs.r0c0}></input></td> 
-                                <td><input id="r0c1" type={"number"} min={1} max={9}></input></td> 
+                <tbody>
+                        <tr>
+                                <td><input id="r0c0" type={"number"} min={1} max={9} ref={refs.r0c0}></input></td> 
+                                <td><input id="r0c1" type={"number"} min={1} max={9} ref={refs.r0c1}></input></td> 
                                 <td><input id="r0c2" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r0c3" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r0c4" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r0c5" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r0c6" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r0c7" type={"number"} min={1} max={9}></input></td> 
-                                <td><input id="r0c8" type={"number"} min={1} max={9}></input></td> 
-
-                        <tr></tr><td><input id="r1c0" type={"number"} min={1} max={9}></input></td> 
+                                <td><input id="r0c8" type={"number"} min={1} max={9}></input></td>                      
+                        </tr> 
+                        <tr>
+                                <td><input id="r1c0" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r1c1" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r1c2" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r1c3" type={"number"} min={1} max={9}></input></td> 
@@ -43,9 +49,11 @@ function Board()
                                 <td><input id="r1c5" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r1c6" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r1c7" type={"number"} min={1} max={9}></input></td> 
-                                <td><input id="r1c8" type={"number"} min={1} max={9}></input></td> 
+                                <td><input id="r1c8" type={"number"} min={1} max={9}></input></td>      
+                        </tr>
 
-                        <tr></tr><td><input id="r2c0" type={"number"} min={1} max={9}></input></td> 
+                        <tr>
+                                <td><input id="r2c0" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r2c1" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r2c2" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r2c3" type={"number"} min={1} max={9}></input></td> 
@@ -53,10 +61,11 @@ function Board()
                                 <td><input id="r2c5" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r2c6" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r2c7" type={"number"} min={1} max={9}></input></td> 
-                                <td><input id="r2c8" type={"number"} min={1} max={9}></input></td> 
-                    </tbody>
-                    <tbody>
-                    <tr></tr> <td><input id="r3c0" type={"number"} min={1} max={9}></input></td> 
+                                <td><input id="r2c8" type={"number"} min={1} max={9}></input></td>
+                        </tr></tbody>
+                <tbody>
+                        <tr>
+                                <td><input id="r3c0" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r3c1" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r3c2" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r3c3" type={"number"} min={1} max={9}></input></td> 
@@ -65,8 +74,10 @@ function Board()
                                 <td><input id="r3c6" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r3c7" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r3c8" type={"number"} min={1} max={9}></input></td> 
+                        </tr> 
 
-                        <tr></tr><td><input id="r4c0" type={"number"} min={1} max={9}></input></td> 
+                        <tr>
+                                <td><input id="r4c0" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r4c1" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r4c2" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r4c3" type={"number"} min={1} max={9}></input></td> 
@@ -74,9 +85,11 @@ function Board()
                                 <td><input id="r4c5" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r4c6" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r4c7" type={"number"} min={1} max={9}></input></td> 
-                                <td><input id="r4c8" type={"number"} min={1} max={9}></input></td> 
+                                <td><input id="r4c8" type={"number"} min={1} max={9}></input></td>                
+                        </tr>
 
-                        <tr></tr><td><input id="r5c0" type={"number"} min={1} max={9}></input></td> 
+                        <tr>
+                                <td><input id="r5c0" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r5c1" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r5c2" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r5c3" type={"number"} min={1} max={9}></input></td> 
@@ -84,10 +97,11 @@ function Board()
                                 <td><input id="r5c5" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r5c6" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r5c7" type={"number"} min={1} max={9}></input></td> 
-                                <td><input id="r5c8" type={"number"} min={1} max={9}></input></td> 
-                    </tbody>
+                                <td><input id="r5c8" type={"number"} min={1} max={9}></input></td>  
+                        </tr></tbody>
                     <tbody>
-                    <tr></tr> <td><input id="r6c0" type={"number"} min={1} max={9}></input></td> 
+                        <tr>
+                                <td><input id="r6c0" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r6c1" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r6c2" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r6c3" type={"number"} min={1} max={9}></input></td> 
@@ -95,9 +109,11 @@ function Board()
                                 <td><input id="r6c5" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r6c6" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r6c7" type={"number"} min={1} max={9}></input></td> 
-                                <td><input id="r6c8" type={"number"} min={1} max={9}></input></td> 
+                                <td><input id="r6c8" type={"number"} min={1} max={9}></input></td>  
+                        </tr> 
 
-                        <tr></tr><td><input id="r7c0" type={"number"} min={1} max={9}></input></td> 
+                        <tr>
+                                <td><input id="r7c0" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r7c1" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r7c2" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r7c3" type={"number"} min={1} max={9}></input></td> 
@@ -105,9 +121,11 @@ function Board()
                                 <td><input id="r7c5" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r7c6" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r7c7" type={"number"} min={1} max={9}></input></td> 
-                                <td><input id="r7c8" type={"number"} min={1} max={9}></input></td> 
+                                <td><input id="r7c8" type={"number"} min={1} max={9}></input></td>    
+                        </tr>
 
-                        <tr></tr><td><input id="r8c0" type={"number"} min={1} max={9}></input></td> 
+                        <tr>
+                                <td><input id="r8c0" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r8c1" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r8c2" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r8c3" type={"number"} min={1} max={9}></input></td> 
@@ -115,12 +133,8 @@ function Board()
                                 <td><input id="r8c5" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r8c6" type={"number"} min={1} max={9}></input></td> 
                                 <td><input id="r8c7" type={"number"} min={1} max={9}></input></td> 
-                                <td><input id="r8c8" type={"number"} min={1} max={9}></input></td> 
-                    </tbody></table></div>
-            <Options />
-        </div>
-
-    )
+                                <td><input id="r8c8" type={"number"} min={1} max={9}></input></td>   
+                        </tr></tbody></table></div>)
 }
 
 export default Board;
--- src/components/Options.js
+++ src/components/Options.js
@@ -2,12 +2,12 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import styles from "./options.module.css";
 
-function Options()
+function Options(props)
 {
     return (<div id={styles.options_div}>
         <Button variant="danger" size="lg">Reset</Button>{' '}
         <Button size="lg">Regenerate</Button>{' '}
-        <Button variant="success"  size="lg">Solve</Button>
+        <Button variant="success"  size="lg" onClick={props.solve}>Solve</Button>
     </div>)
 }
 
