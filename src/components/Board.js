4fd0d148276eb276c20df16f0fd52c45ee8f72f2
Charptr0 <charlesofficial1@hotmail.com>
2022-04-17 22:24:41 -0400

    finished validate function

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,9 +2,7 @@ import React, { useEffect, useRef } from "react";
 import styles from "./board.module.css";
 import Options from "./Options";
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
+function sleep(ms) {return new Promise(resolve => setTimeout(resolve, ms))}
 
 function flatten(data)
 {
@@ -21,21 +19,114 @@ function flatten(data)
 
 function isValidSquare(board, row, col)
 {	
-	let temp = []
+	let nums = []
+
+	function validateNumbers() {
+		nums.sort();
+	
+		for(let i = 0; i < nums.length; i++) {
+			if(nums[i] !== (i + 1)) return false;
+		}
+
+		nums = [];
+
+		return true
+	}
 
 	// check row
 	for(let i = 0; i < board[row].length; i++) {
 		if(board[row][i] === 0) return false;
-		else temp.push(board[row][i]);
+		else nums.push(board[row][i]);
+	} if(!validateNumbers()) return false;
+
+	// check cols
+	for(let i = 0; i < board.length; i++) {
+		if(board[i][col] === 0) return false;
+		else nums.push(board[i][col]);
+	} if(!validateNumbers()) return false;
+
+	// check the first square
+	if(row <= 2 && col <= 2) {
+		nums.push(board[0][0], board[0][1], board[0][2], 
+			board[1][0], board[1][1], board[1][2],
+			board[2][0], board[2][1], board[2][2]);
+
+		return validateNumbers();
 	}
 
-	temp.sort();
+	// check the second square
+	else if(col >= 3 && col <= 5 && row <= 2) {
+		nums.push(board[0][3], board[0][4], board[0][5], 
+			board[1][3], board[1][4], board[1][5],
+			board[2][3], board[2][4], board[2][5]);
 
-	for(let i = 0; i < temp.length; i++) {
-		if(temp[i] !== (i + 1)) return false;
+		return validateNumbers();
 	}
 
-	return true;
+	// check the third square
+	else if(col >= 6 && col <= 8 && row <= 2) {
+		nums.push(board[0][6], board[0][7], board[0][8], 
+			board[1][6], board[1][7], board[1][8],
+			board[2][6], board[2][7], board[2][8]);
+
+		return validateNumbers();
+	}
+
+	// check the fourth square
+	else if(row >= 3 && row <= 5 && col <= 2) {
+		nums.push(board[3][0], board[3][1], board[3][2], 
+			board[4][0], board[4][1], board[4][2],
+			board[5][0], board[5][1], board[5][2]);
+
+		return validateNumbers();
+	}
+
+	// check the fifth square
+	else if(row >= 3 && row <= 5 && col >= 3 && col <= 5) {
+		nums.push(board[3][3], board[3][4], board[3][5], 
+			board[4][3], board[4][4], board[4][5],
+			board[5][3], board[5][4], board[5][5]);
+
+		return validateNumbers();
+	}
+
+	// check the sixth square
+	else if(row >= 3 && row <= 5 && col >= 6 && col <= 8) {
+		nums.push(board[3][6], board[3][7], board[3][8], 
+			board[4][6], board[4][7], board[4][8],
+			board[5][6], board[5][7], board[5][8]);
+
+		return validateNumbers();
+	}
+
+	// check the seventh square
+	else if(row >= 6 && row <= 8 && col <= 2) {
+		nums.push(board[6][0], board[6][1], board[6][2], 
+			board[7][0], board[7][1], board[7][2],
+			board[8][0], board[8][1], board[8][2]);
+
+		return validateNumbers();
+	}
+
+	// check the eighth square
+	else if(row >= 6 && row <= 8 && col >= 3 && col <= 5) {
+		nums.push(board[6][3], board[6][4], board[6][5], 
+			board[7][3], board[7][4], board[7][5],
+			board[8][3], board[8][4], board[8][5]);
+
+		return validateNumbers();
+	}
+
+	// check the ninth square
+	else if(row >= 6 && row <= 8 && col >= 6 && col <= 8) {
+		nums.push(board[6][6], board[6][7], board[6][8], 
+			board[7][6], board[7][7], board[7][8],
+			board[8][6], board[8][7], board[8][8]);
+
+		return validateNumbers();
+	}
+
+	return false;
 }
 
 function Board()
@@ -71,15 +162,15 @@ function Board()
 
 	useEffect(() => {
 		const data = [
-			[3,6,5,2,9,8,1,4,7],
-			[0,2,1,0,7,0,0,0,0],
-			[0,3,4,9,1,5,0,8,6],
-			[6,0,0,5,0,0,3,1,0],
-			[5,0,7,0,0,0,0,2,8],
-			[0,0,8,0,9,0,0,0,0],
-			[1,0,0,0,0,0,0,0,3],
-			[0,0,0,0,6,9,8,7,0],
-			[0,0,6,8,0,3,0,0,0]];
+			[8,2,7,1,5,4,3,9,6],
+			[9,6,5,3,2,7,1,4,8],
+			[3,4,1,6,8,9,7,5,2],
+			[5,9,3,4,6,8,2,7,1],
+			[4,7,2,5,1,3,6,8,9],
+			[6,1,8,9,7,2,4,3,5],
+			[7,8,6,2,3,5,9,1,4],
+			[1,5,4,7,9,6,8,2,3],
+			[2,3,9,8,4,1,5,6,7]];
 		
 		const flattenBoard = flatten(data);
 
@@ -108,7 +199,6 @@ function Board()
 	function validate()
 	{
 		const board = []
-		
 		let row = []
 
 		Object.keys(refs).forEach((k) => {
@@ -128,6 +218,9 @@ function Board()
 		// push the last row
 		board.push(row);
 
+		/**
+		 * ValidHandler, but with support for the wait function
+		 */
 		async function validateHelper()
 		{
 			for(let i = 0; i < board.length; i++) {
@@ -137,7 +230,7 @@ function Board()
 					refs[id].current.style.backgroundColor = "orange";
 
 					if(isValidSquare(board, i, j)) {					
-						await sleep(1000); 
+						await sleep(100); 
 						refs[id].current.style.backgroundColor = "green";
 					}
 
